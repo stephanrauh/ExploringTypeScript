830da0a2c8ed2072156a01c4733d56c1fade45fa
stephan <webmaster1@beyondjava.de>
2016-05-16 13:52:25 +0200

    started to add the descriptions

--- wheel/wheel.js
+++ wheel/wheel.js
@@ -1,18 +1,38 @@
 /// <reference path="./d3.d.ts"/>;
 var Wheel = (function () {
     function Wheel() {
-        var w = window.innerWidth;
-        this._size = window.innerHeight;
-        if (w < this.size) {
+        var w = document.getElementById("wheel").clientWidth;
+        this._size = document.getElementById("wheel").clientHeight;
+        ;
+        if (w < this._size) {
             this._size = w;
         }
+        console.log(this._size);
+        this.chart = d3.select("#wheel").append("svg:svg")
+            .attr("class", "chart")
+            .attr("width", w)
+            .attr("height", this._size).append("svg:g")
+            .attr("transform", "translate(400,300) rotate(-35)");
+        this.readData();
     }
     Object.defineProperty(Wheel.prototype, "size", {
         get: function () { return this._size; },
         enumerable: true,
         configurable: true
     });
-    Wheel.prototype.drawArc = function (innerRadius, outerRadius, startAngle, endAngle, color) {
+    Wheel.prototype.readData = function () {
+        var _this = this;
+        var request = new XMLHttpRequest();
+        request.onload = function () {
+            if (request.readyState == 4 && request.status == 200) {
+                var segments = JSON.parse(request.responseText);
+                _this.showWheel(segments);
+            }
+        };
+        request.open("get", "wheel.json", true);
+        request.send();
+    };
+    Wheel.prototype.drawArc = function (innerRadius, outerRadius, startAngle, endAngle, color, name) {
         var arc = d3.svg.arc()
             .innerRadius(innerRadius)
             .outerRadius(outerRadius)
@@ -24,39 +44,45 @@ var Wheel = (function () {
             return color;
         })
             .attr("d", arc);
+        var x = (innerRadius + 5) * Math.sin((startAngle + endAngle) / 2);
+        var y = (innerRadius + 5) + Math.cos((startAngle + endAngle) / 2);
+        this.chart
+            .append("text")
+            .property("x", x)
+            .property("y", y)
+            .property("font-family", "sans-serif")
+            .property("font-size", "10px")
+            .property("fill", "red")
+            .text(name);
     };
-    Wheel.prototype.drawSegment = function (level, startAngle, endAngle) {
-        var innerRadius = this.size * level / 5 / 2 / 2;
-        var outerRadius = this.size * (level + 1) / 5 / 2 / 2;
-        var color = d3.hsl(startAngle * 180 / Math.PI, 0.50, 0.50).rgb();
-        this.drawArc(innerRadius, outerRadius, startAngle, endAngle, color.toString());
+    Wheel.prototype.drawSegment = function (level, startAngle, endAngle, name) {
+        var innerRadius = this._size * level / 5 / 2;
+        if (level < 1) {
+            innerRadius = 0;
+        }
+        var outerRadius = this.size * (level + 1) / 5 / 2;
+        var color = d3.hsl((startAngle + endAngle) * 90 * level / Math.PI, 0.3 + level / 5, 0.50).rgb();
+        this.drawArc(innerRadius, outerRadius, startAngle, endAngle, color.toString(), name);
     };
-    Wheel.prototype.showWheel = function () {
-        this.chart = d3.select("#wheel").append("svg:svg")
-            .attr("class", "chart")
-            .attr("width", this.size)
-            .attr("height", this.size).append("svg:g")
-            .attr("transform", "translate(200,200)");
-        this.drawSegment(1, 0, 1);
-        this.drawSegment(1, 1, 2);
-        this.drawSegment(1, 2, 3);
-        this.drawSegment(1, 3, 4);
-        this.drawSegment(1, 4, 5);
-        this.drawSegment(1, 5, Math.PI * 2);
-        this.drawSegment(2, 0, 0.5);
-        this.drawSegment(2, 0.5, 1);
-        this.drawSegment(2, 1, 1.5);
-        this.drawSegment(2, 1.5, 2);
-        this.drawSegment(2, 2, 2.5);
-        this.drawSegment(2, 2.5, 3);
-        this.drawSegment(2, 3, 3.5);
-        this.drawSegment(2, 3.5, 4);
-        this.drawSegment(2, 4, 4.5);
-        this.drawSegment(2, 4.5, 5);
-        this.drawSegment(2, 5, 5.5);
-        this.drawSegment(2, 5.5, Math.PI * 2);
+    Wheel.prototype.showWheel = function (segments, level, startAngle, endAngle) {
+        if (level === void 0) { level = 0; }
+        if (startAngle === void 0) { startAngle = 0; }
+        if (endAngle === void 0) { endAngle = Math.PI * 2; }
+        var count = segments.length;
+        var startSection = startAngle;
+        var endSection = startSection;
+        var range = endAngle - startAngle;
+        for (var _i = 0, segments_1 = segments; _i < segments_1.length; _i++) {
+            var segment = segments_1[_i];
+            endSection += range / count;
+            this.drawSegment(level, startSection, endSection, segment.name);
+            if (segment.children && segment.children.length > 0) {
+                this.showWheel(segment.children, level + 1, startSection, endSection);
+            }
+            startSection = endSection;
+        }
     };
     return Wheel;
 }());
-new Wheel().showWheel();
+new Wheel();
 //# sourceMappingURL=wheel.js.map
--- wheel/wheel.ts
+++ wheel/wheel.ts
@@ -1,5 +1,12 @@
 /// <reference path="./d3.d.ts"/>;
 
+interface Segment {
+    name: string;
+    colour: string;
+    children: Segment[];
+}
+
+
 class Wheel {
   _size: number;
   chart: d3.Selection<any>;
@@ -7,64 +14,86 @@ class Wheel {
   get size(): number { return this._size }
 
     constructor() {
-      var w = window.innerWidth;
-      this._size = window.innerHeight;
-      if (w < this.size) {
+
+      var w = document.getElementById("wheel").clientWidth;
+      this._size = document.getElementById("wheel").clientHeight;;
+      if (w < this._size) {
         this._size=w;
       }
+      console.log(this._size)
+      this.chart = d3.select("#wheel").append("svg:svg")
+          .attr("class", "chart")
+          .attr("width", w)
+          .attr("height", this._size).append("svg:g")
+          .attr("transform", "translate(400,300) rotate(-35)")
+          ;
+      this.readData();
     }
 
-    drawArc(innerRadius: number, outerRadius: number, startAngle:number, endAngle:number, color: string) {
+    readData() {
+      var request = new XMLHttpRequest();
+      request.onload = () => {
+        if (request.readyState==4 && request.status==200)
+        {
+          var segments: Segment[] = JSON.parse(request.responseText);
+          this.showWheel(segments);
+        }
+      };
+      request.open("get", "wheel.json", true);
+      request.send();
+    }
+
+    drawArc(innerRadius: number, outerRadius: number, startAngle:number, endAngle:number, color: string, name: string) {
       var arc = d3.svg.arc()
           .innerRadius(innerRadius)
           .outerRadius(outerRadius)
           .startAngle(startAngle)
           .endAngle(endAngle);
-          this.chart
-              .append("svg:path")
-              .attr("fill", function(d: any, index: number, outerindex: number) {
-                  return color;
-              })
-              .attr("d", arc);
+      this.chart
+          .append("svg:path")
+          .attr("fill", function(d: any, index: number, outerindex: number) {
+              return color;
+          })
+          .attr("d", arc);
 
+      var x = (innerRadius + 5) * Math.sin((startAngle+endAngle)/2)
+      var y = (innerRadius + 5) + Math.cos((startAngle+endAngle)/2)
+      this.chart
+          .append("text")
+
+          .property("x", x)
+          .property("y", y)
+          .property("font-family", "sans-serif")
+          .property("font-size", "10px")
+          .property("fill", "red")
+          .text(name)
     }
 
-    drawSegment(level: number, startAngle:number, endAngle:number) {
-      var innerRadius = this.size * level / 5 / 2 / 2;
-      var outerRadius = this.size * (level+1) / 5 / 2 / 2;
-      var color = d3.hsl(startAngle*180/Math.PI, 0.50, 0.50).rgb();
-      this.drawArc(innerRadius, outerRadius, startAngle, endAngle, color.toString());
+    drawSegment(level: number, startAngle:number, endAngle:number, name:string) {
+      var innerRadius = this._size * level / 5 / 2;
+      if (level<1) {
+        innerRadius=0;
+      }
+      var outerRadius = this.size * (level+1) / 5 / 2;
+      var color = d3.hsl((startAngle+endAngle)*90*level/Math.PI, 0.3 + level / 5, 0.50).rgb();
+      this.drawArc(innerRadius, outerRadius, startAngle, endAngle, color.toString(), name);
     }
 
 
-    showWheel() {
-        this.chart = d3.select("#wheel").append("svg:svg")
-            .attr("class", "chart")
-            .attr("width", this.size)
-            .attr("height", this.size).append("svg:g")
-            .attr("transform", "translate(200,200)")
-            ;
-            this.drawSegment(1, 0, 1)
-            this.drawSegment(1, 1, 2)
-            this.drawSegment(1, 2, 3)
-            this.drawSegment(1, 3, 4)
-            this.drawSegment(1, 4, 5)
-            this.drawSegment(1, 5, Math.PI * 2)
-
-            this.drawSegment(2, 0, 0.5)
-            this.drawSegment(2, 0.5, 1)
-            this.drawSegment(2, 1, 1.5)
-            this.drawSegment(2, 1.5, 2)
-            this.drawSegment(2, 2, 2.5)
-            this.drawSegment(2, 2.5, 3)
-            this.drawSegment(2, 3, 3.5)
-            this.drawSegment(2, 3.5, 4)
-            this.drawSegment(2, 4, 4.5)
-            this.drawSegment(2, 4.5, 5)
-            this.drawSegment(2, 5, 5.5)
-            this.drawSegment(2, 5.5, Math.PI * 2)
-
+    showWheel(segments: Segment[], level: number = 0, startAngle: number = 0, endAngle: number = Math.PI *2 ) {
+      var count = segments.length
+      var startSection = startAngle
+      var endSection = startSection
+      var range = endAngle - startAngle
+      for (var segment of segments) {
+        endSection += range / count
+        this.drawSegment(level, startSection , endSection, segment.name)
+        if (segment.children && segment.children.length > 0) {
+          this.showWheel(segment.children, level+1, startSection, endSection)
+        }
+        startSection = endSection
+      }
     }
 }
 
-new Wheel().showWheel();
+new Wheel();
