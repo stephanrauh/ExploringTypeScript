a5e9e598b3b9e3c161f9e9dc234bf73345bee87e
stephan <webmaster1@beyondjava.de>
2016-05-16 18:17:30 +0200

    added texts to the wheel following the d3 style

--- wheel/wheel.js
+++ wheel/wheel.js
@@ -1,18 +1,19 @@
 /// <reference path="./d3.d.ts"/>;
 var Wheel = (function () {
     function Wheel() {
+        this._maxDepth = 0;
         var w = document.getElementById("wheel").clientWidth;
         this._size = document.getElementById("wheel").clientHeight;
         ;
         if (w < this._size) {
             this._size = w;
         }
-        console.log(this._size);
         this.chart = d3.select("#wheel").append("svg:svg")
             .attr("class", "chart")
-            .attr("width", w)
-            .attr("height", this._size).append("svg:g")
-            .attr("transform", "translate(400,300) rotate(-35)");
+            .attr("viewBox", "-100 -100 200 200")
+            .attr("width", this._size)
+            .attr("height", this._size)
+            .append("svg:g");
         this.readData();
     }
     Object.defineProperty(Wheel.prototype, "size", {
@@ -26,60 +27,121 @@ var Wheel = (function () {
         request.onload = function () {
             if (request.readyState == 4 && request.status == 200) {
                 var segments = JSON.parse(request.responseText);
-                _this.showWheel(segments);
+                _this.showWheelFunctional(segments);
             }
         };
         request.open("get", "wheel.json", true);
         request.send();
     };
-    Wheel.prototype.drawArc = function (innerRadius, outerRadius, startAngle, endAngle, color, name) {
-        var arc = d3.svg.arc()
-            .innerRadius(innerRadius)
-            .outerRadius(outerRadius)
-            .startAngle(startAngle)
-            .endAngle(endAngle);
-        this.chart
-            .append("svg:path")
-            .attr("fill", function (d, index, outerindex) {
-            return color;
-        })
-            .attr("d", arc);
-        var x = (innerRadius + 5) * Math.sin((startAngle + endAngle) / 2);
-        var y = (innerRadius + 5) + Math.cos((startAngle + endAngle) / 2);
-        this.chart
-            .append("text")
-            .property("x", x)
-            .property("y", y)
-            .property("font-family", "sans-serif")
-            .property("font-size", "10px")
-            .property("fill", "red")
-            .text(name);
-    };
-    Wheel.prototype.drawSegment = function (level, startAngle, endAngle, name) {
-        var innerRadius = this._size * level / 5 / 2;
-        if (level < 1) {
-            innerRadius = 0;
-        }
-        var outerRadius = this.size * (level + 1) / 5 / 2;
-        var color = d3.hsl((startAngle + endAngle) * 90 * level / Math.PI, 0.3 + level / 5, 0.50).rgb();
-        this.drawArc(innerRadius, outerRadius, startAngle, endAngle, color.toString(), name);
-    };
-    Wheel.prototype.showWheel = function (segments, level, startAngle, endAngle) {
-        if (level === void 0) { level = 0; }
-        if (startAngle === void 0) { startAngle = 0; }
-        if (endAngle === void 0) { endAngle = Math.PI * 2; }
-        var count = segments.length;
-        var startSection = startAngle;
-        var endSection = startSection;
-        var range = endAngle - startAngle;
-        for (var _i = 0, segments_1 = segments; _i < segments_1.length; _i++) {
-            var segment = segments_1[_i];
-            endSection += range / count;
-            this.drawSegment(level, startSection, endSection, segment.name);
-            if (segment.children && segment.children.length > 0) {
-                this.showWheel(segment.children, level + 1, startSection, endSection);
+    Wheel.prototype.showWheelFunctional = function (segments) {
+        var _this = this;
+        var tree = d3.layout.tree()
+            .size([this._size, this._size]);
+        var root = segments[0];
+        this.calculateTreeDepth(root);
+        this.calculateTreeSize(root);
+        this.calculateSegmentSize(root, 0, Math.PI * 2);
+        var nodes = tree.nodes(root);
+        nodes.forEach(function (node) {
+            var w = node;
+            w.innerRadius = w.depth == 0 ? 0 : (100 * w.depth) / (_this._maxDepth + 2);
+            w.outerRadius = 100 * (1 + w.depth) / (_this._maxDepth + 2);
+            if (w.depth == _this._maxDepth) {
+                w.outerRadius = 100;
             }
-            startSection = endSection;
+            w.x = 0;
+            w.y = 0;
+        });
+        var node = this.chart.selectAll("g.node")
+            .data(nodes);
+        var nodeEnter = node.enter().append("svg:path")
+            .attr("fill", function (d) { return _this.color(d); })
+            .attr("d", function (d) { return _this.drawArc(d); });
+        var textEnter = node.enter().append("text")
+            .attr("x", 0)
+            .attr("y", function (d) { return 0; })
+            .attr("dy", ".15em")
+            .attr("text-anchor", "start")
+            .text(function (d) { return d.name; })
+            .style("fill-opacity", 1)
+            .attr("transform", function (d) { return _this.textPosition(d); });
+    };
+    Wheel.prototype.textPosition = function (d) {
+        var radius = d.innerRadius + ((d.outerRadius - d.innerRadius) / 2) - 3;
+        var dy = -d.name.length;
+        var textRotation = 90;
+        if (d.depth == this._maxDepth) {
+            textRotation = 0;
+            dy = 0;
+            radius = d.innerRadius + 5;
+        }
+        if (d.depth == 0) {
+            textRotation = -90;
+            dy = 0;
+            radius = 0;
+            return "translate(-12)";
+        }
+        return "rotate(" + (d.angleInDegrees) + ") translate(" + radius + ", " + dy + ")"
+            + "rotate(" + textRotation + ")";
+    };
+    Wheel.prototype.drawArc = function (d) {
+        var segment = d;
+        var result = d3.svg.arc()
+            .innerRadius(function (a) { return segment.innerRadius; })
+            .outerRadius(function (a) { return segment.outerRadius; })
+            .startAngle(function (a) { return segment.startAngle; })
+            .endAngle(function (a) { return segment.endAngle; });
+        return result();
+    };
+    Wheel.prototype.color = function (d) {
+        return d3.hsl((d.startAngle + d.endAngle) * 90 * d.depth / Math.PI, 0.3 + d.depth / 5, 0.50).rgb().toString();
+    };
+    ;
+    Wheel.prototype.calculateTreeDepth = function (segment, level) {
+        if (level === void 0) { level = 0; }
+        if (segment.children) {
+            for (var _i = 0, _a = segment.children; _i < _a.length; _i++) {
+                var child = _a[_i];
+                this.calculateTreeDepth(child, level + 1);
+            }
+        }
+        else {
+            if (level > this._maxDepth) {
+                this._maxDepth = level;
+            }
+        }
+    };
+    Wheel.prototype.calculateTreeSize = function (segment, level) {
+        var _this = this;
+        if (level === void 0) { level = 0; }
+        segment.treeSize = 0;
+        if (segment.children) {
+            segment.children.forEach(function (segment) { return _this.calculateTreeSize(segment, level + 1); });
+            for (var _i = 0, _a = segment.children; _i < _a.length; _i++) {
+                var child = _a[_i];
+                segment.treeSize += child.treeSize;
+            }
+        }
+        else {
+            segment.treeSize = (this._maxDepth - level + 1);
+        }
+    };
+    Wheel.prototype.calculateSegmentSize = function (segment, startAngle, endAngle) {
+        segment.startAngle = startAngle;
+        segment.endAngle = endAngle;
+        var center = segment.startAngle + ((segment.endAngle - segment.startAngle) / 2);
+        segment.angleInDegrees = (center * 180 / Math.PI) - 90;
+        console.log(segment.angleInDegrees + " " + segment.name);
+        //  segment.angleInDegrees = (segment.startAngle) * 180 / Math.PI-90;
+        if (segment.children) {
+            var slice = (segment.endAngle - segment.startAngle) / segment.treeSize;
+            var start = startAngle;
+            for (var _i = 0, _a = segment.children; _i < _a.length; _i++) {
+                var child = _a[_i];
+                var end = start + child.treeSize * slice;
+                this.calculateSegmentSize(child, start, end);
+                start = end;
+            }
         }
     };
     return Wheel;
--- wheel/wheel.ts
+++ wheel/wheel.ts
@@ -2,13 +2,30 @@
 
 interface Segment {
     name: string;
-    colour: string;
+    color: string;
+    treeSize: number;
+    startAngle: number;
+    endAngle: number;
     children: Segment[];
+    angleInDegrees: number;
+}
+
+interface WheelNode extends d3.layout.tree.Node {
+  name: string;
+  color: string;
+  treeSize: number;
+  startAngle: number;
+  endAngle: number;
+  id: number;
+  innerRadius: number;
+  outerRadius: number;
+  angleInDegrees: number;
 }
 
 
 class Wheel {
   _size: number;
+  _maxDepth = 0;
   chart: d3.Selection<any>;
 
   get size(): number { return this._size }
@@ -20,12 +37,12 @@ class Wheel {
       if (w < this._size) {
         this._size=w;
       }
-      console.log(this._size)
       this.chart = d3.select("#wheel").append("svg:svg")
           .attr("class", "chart")
-          .attr("width", w)
-          .attr("height", this._size).append("svg:g")
-          .attr("transform", "translate(400,300) rotate(-35)")
+          .attr("viewBox", "-100 -100 200 200")
+          .attr("width", this._size)
+          .attr("height", this._size)
+          .append("svg:g")
           ;
       this.readData();
     }
@@ -36,62 +53,128 @@ class Wheel {
         if (request.readyState==4 && request.status==200)
         {
           var segments: Segment[] = JSON.parse(request.responseText);
-          this.showWheel(segments);
+          this.showWheelFunctional(segments);
         }
       };
       request.open("get", "wheel.json", true);
       request.send();
     }
 
-    drawArc(innerRadius: number, outerRadius: number, startAngle:number, endAngle:number, color: string, name: string) {
-      var arc = d3.svg.arc()
-          .innerRadius(innerRadius)
-          .outerRadius(outerRadius)
-          .startAngle(startAngle)
-          .endAngle(endAngle);
-      this.chart
-          .append("svg:path")
-          .attr("fill", function(d: any, index: number, outerindex: number) {
-              return color;
-          })
-          .attr("d", arc);
+    showWheelFunctional(segments: Segment[]) {
+      var tree = d3.layout.tree()
+      	.size([this._size, this._size]);
+      var root = segments[0]
+      this.calculateTreeDepth(root)
+      this.calculateTreeSize(root)
+      this.calculateSegmentSize(root, 0, Math.PI * 2)
 
-      var x = (innerRadius + 5) * Math.sin((startAngle+endAngle)/2)
-      var y = (innerRadius + 5) + Math.cos((startAngle+endAngle)/2)
-      this.chart
-          .append("text")
-
-          .property("x", x)
-          .property("y", y)
-          .property("font-family", "sans-serif")
-          .property("font-size", "10px")
-          .property("fill", "red")
-          .text(name)
-    }
-
-    drawSegment(level: number, startAngle:number, endAngle:number, name:string) {
-      var innerRadius = this._size * level / 5 / 2;
-      if (level<1) {
-        innerRadius=0;
-      }
-      var outerRadius = this.size * (level+1) / 5 / 2;
-      var color = d3.hsl((startAngle+endAngle)*90*level/Math.PI, 0.3 + level / 5, 0.50).rgb();
-      this.drawArc(innerRadius, outerRadius, startAngle, endAngle, color.toString(), name);
-    }
-
-
-    showWheel(segments: Segment[], level: number = 0, startAngle: number = 0, endAngle: number = Math.PI *2 ) {
-      var count = segments.length
-      var startSection = startAngle
-      var endSection = startSection
-      var range = endAngle - startAngle
-      for (var segment of segments) {
-        endSection += range / count
-        this.drawSegment(level, startSection , endSection, segment.name)
-        if (segment.children && segment.children.length > 0) {
-          this.showWheel(segment.children, level+1, startSection, endSection)
+      var nodes = tree.nodes(root)
+      nodes.forEach(node => {
+        let w = <WheelNode>node;
+        w.innerRadius = w.depth == 0 ? 0 : (100 * w.depth) / (this._maxDepth+2);
+        w.outerRadius = 100 * (1+w.depth) / (this._maxDepth+2);
+        if (w.depth == this._maxDepth) {
+          w.outerRadius = 100;
         }
-        startSection = endSection
+        w.x=0;
+        w.y=0;
+      });
+      var node = this.chart.selectAll("g.node")
+        .data(nodes);
+
+
+      var nodeEnter = node.enter().append("svg:path")
+            .attr("fill", d => this.color(<WheelNode>d))
+            .attr("d", (d) => this.drawArc(d));
+
+      var textEnter = node.enter().append("text")
+          .attr("x", 0)
+          .attr("y", d => 0)
+      	  .attr("dy", ".15em")
+      	  .attr("text-anchor", "start")
+      	  .text(d => (<WheelNode>d).name)
+      	  .style("fill-opacity", 1)
+          .attr("transform", d => this.textPosition(<WheelNode>d))
+    }
+
+    textPosition(d: WheelNode) : string {
+      var radius = d.innerRadius + ((d.outerRadius-d.innerRadius) / 2) - 3
+      var dy = -(<WheelNode>d).name.length
+      var textRotation = 90;
+      if (d.depth == this._maxDepth) {
+        textRotation = 0
+        dy = 0
+        radius = d.innerRadius +5
+      }
+      if (d.depth == 0) {
+        textRotation = -90;
+        dy = 0
+        radius = 0
+        return "translate(-12)"
+      }
+
+      return "rotate(" +((<WheelNode>d).angleInDegrees) + ") translate(" + radius + ", " + dy +")"
+            + "rotate(" + textRotation +")";
+    }
+
+    drawArc (d: d3.layout.tree.Node): string {
+      var segment = <WheelNode>d;
+      var result = d3.svg.arc()
+          .innerRadius(a => { return segment.innerRadius; })
+          .outerRadius(a => { return segment.outerRadius;})
+          .startAngle(a => segment.startAngle)
+          .endAngle(a => segment.endAngle)
+          ;
+      return result();
+    }
+
+    color(d: WheelNode) {
+        return d3.hsl((d.startAngle+d.endAngle)*90*d.depth/Math.PI, 0.3 + d.depth / 5, 0.50).rgb().toString();
+    };
+
+    calculateTreeDepth(segment: Segment, level: number=0) {
+      if (segment.children) {
+        for (var child of segment.children) {
+          this.calculateTreeDepth(child, level+1)
+        }
+      }
+      else {
+        if (level > this._maxDepth) {
+          this._maxDepth = level;
+        }
+      }
+    }
+
+
+    calculateTreeSize(segment: Segment, level: number=0) {
+      segment.treeSize = 0;
+      if (segment.children) {
+        segment.children.forEach(segment => this.calculateTreeSize(segment, level+1))
+        for (var child of segment.children) {
+          segment.treeSize += child.treeSize
+        }
+      }
+      else {
+        segment.treeSize = (this._maxDepth-level+1);
+      }
+    }
+
+    calculateSegmentSize(segment: Segment, startAngle: number, endAngle: number) {
+      segment.startAngle = startAngle;
+      segment.endAngle = endAngle;
+      var center = segment.startAngle + ((segment.endAngle - segment.startAngle) / 2)
+      segment.angleInDegrees = (center * 180 / Math.PI)-90;
+      console.log(segment.angleInDegrees + " " + segment.name)
+    //  segment.angleInDegrees = (segment.startAngle) * 180 / Math.PI-90;
+      if (segment.children) {
+        var slice = (segment.endAngle - segment.startAngle) / segment.treeSize
+        var start = startAngle;
+        for (var child of segment.children) {
+          var end = start + child.treeSize * slice;
+          this.calculateSegmentSize(child, start, end)
+          start = end
+
+        }
       }
     }
 }
